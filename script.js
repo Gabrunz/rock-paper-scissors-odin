9f68aa719543425b1c806420dc267e0ea6ac5ed6
Gabriele Mora <gabriele.mora@live.it>
2023-01-25 17:58:27 +0100

    add choice for user & check input of the user

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 function getComputerChoice() {
-    const choice = ["Rock", "Paper", "Scissors"];
-    return choice[Math.floor(Math.random() * 3)]; 
+    const computerChoice = ["rock", "paper", "scissors"];
+    return computerChoice[Math.floor(Math.random() * 3)]; 
 }
 
-console.log(getComputerChoice());
+function getPlayerChoice() {
+    notCorrectInput = true;
+    while(notCorrectInput) {
+        let playerChoice = prompt("Rock, Paper or Scissors ?", "Rock");
+        let playerChoiceLow = playerChoice.toLowerCase();
+        if(playerChoiceLow === "rock" || playerChoiceLow === "paper" || playerChoiceLow === "scissors")
+        {
+            return playerChoiceLow;
+        }
+        else {
+            window.alert("Wrong input, please retry");
+        }
+    }
+    
+}
+
